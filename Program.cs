985cd457e9ee52dd6bfdcad8ef07bcf6dc786540
sumryooo <adha.surya2002@gmail.com>
2022-03-19 16:56:15 +0700

    menambahkan  class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Mod4_1302204026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDataBase<int> sdb = new SimpleDataBase<int>();
+
+            sdb.AddNewData(13); sdb.AddNewData(02); sdb.AddNewData(20);
+            sdb.PrintAllData();
+        }
+    }
+}
